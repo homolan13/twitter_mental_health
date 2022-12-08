3396c63887f041d24ea8de4e8cd258f3a42df214
homolan13 <93644446+homolan13@users.noreply.github.com>
2022-12-08 16:18:01 +0100

    finito

--- bert.py
+++ bert.py
@@ -62,18 +62,22 @@ def main(batch_size, n_epochs, max_patience):
     attention_masks = th.cat(attention_masks, dim = 0)
     labels = th.tensor(data['label'].values)
 
-    # Create train and test sets and prepare DataLoader
-    train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.3, shuffle = True, stratify = labels)
+    # Create train, validation and test sets and prepare DataLoader
+    train_val_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.3, shuffle=True, stratify=labels)
+    train_idx, val_idx = train_test_split(train_val_idx, test_size=0.2, shuffle=True, stratify=labels[train_val_idx])
     train_set = TensorDataset(token_id[train_idx], attention_masks[train_idx], labels[train_idx])
     train_dataloader = DataLoader(train_set, sampler=RandomSampler(train_set), batch_size=batch_size)
+    val_set = TensorDataset(token_id[val_idx], attention_masks[val_idx], labels[val_idx])
+    val_dataloader = DataLoader(val_set, sampler=RandomSampler(val_set), batch_size=batch_size)
     test_set = TensorDataset(token_id[test_idx], attention_masks[test_idx], labels[test_idx])
     test_dataloader = DataLoader(test_set, sampler=SequentialSampler(test_set), batch_size=batch_size)
 
-    # Prepare training and output
+    # Prepare training and validation/test output
     patience = max_patience
     best_accuracy = 0 # Keep track of best performance for patience
     it = 0
-    output = {k: [] for k in ['training_loss', 'accuracy', 'precision', 'recall', 'f1']}
+    val_output = {k: [] for k in ['training_loss', 'accuracy', 'precision', 'recall', 'f1']}
+    test_output = {k: 'NaN' for k in ['accuracy', 'precision', 'recall', 'f1']}
 
     for epoch in range(n_epochs):
         
@@ -93,7 +97,7 @@ def main(batch_size, n_epochs, max_patience):
             optimizer.step()
             # Log loss every 20th iteration
             if it%20 == 0:
-                output['training_loss'].append((it, train_output.loss.item()))
+                val_output['training_loss'].append((it, train_output.loss.item()))
 
             # Log metrics every 100th iteration
             if it%100 == 0:
@@ -106,7 +110,7 @@ def main(batch_size, n_epochs, max_patience):
                 batch_recall = []
                 batch_f1 = []
 
-                for batch in test_dataloader:
+                for batch in val_dataloader:
                     batch = tuple(t.to(device) for t in batch)
                     b_input_ids, b_input_mask, b_labels = batch
                     with th.no_grad():
@@ -122,18 +126,19 @@ def main(batch_size, n_epochs, max_patience):
                     batch_recall.append(b_recall)
                     batch_f1.append(b_f1)
 
-                output['accuracy'].append(np.array(batch_accuracy).mean())
-                output['precision'].append(np.array(batch_precision).mean())
-                output['recall'].append(np.array(batch_recall).mean())
-                output['f1'].append(np.array(batch_f1).mean())
+                val_output['accuracy'].append((it, np.array(batch_accuracy).mean()))
+                val_output['precision'].append((it, np.array(batch_precision).mean()))
+                val_output['recall'].append((it, np.array(batch_recall).mean()))
+                val_output['f1'].append((it, np.array(batch_f1).mean()))
                 
-                print(f'Loss: {output["training_loss"][-1][1]:.3f}')
-                print(f'Accuracy: {output["accuracy"][-1]:.3f}')
+                print(f'Loss: {val_output["training_loss"][-1][1]:.3f}')
+                print(f'Accuracy: {val_output["accuracy"][-1][1]:.3f}')
 
                 # Early stopping
-                if output['accuracy'][-1] > best_accuracy:
-                    best_accuracy = output['accuracy'][-1]
-                    th.save(model.state_dict(), './output/bert_model_ft.pt')
+                if val_output['accuracy'][-1][1] > best_accuracy:
+                    best_accuracy = val_output['accuracy'][-1][1]
+                    best_model_state = model.state_dict()
+                    th.save(best_model_state, './output/bert_model_ft.pt')
                     patience = max_patience
                     print(f'Resetting patience to {max_patience}')
                 else:
@@ -148,11 +153,51 @@ def main(batch_size, n_epochs, max_patience):
             continue
         break
 
-    output['n_epochs'] = epoch
+    val_output['n_epochs'] = epoch
+
+    ### TESTING ###    
+    # Set best model to evaluation mode
+    best_model = model
+    best_model.load_state_dict(best_model_state)
+    best_model.eval()
+    # Tracking variables 
+    batch_accuracy = []
+    batch_precision = []
+    batch_recall = []
+    batch_f1 = []
+
+    for batch in test_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        with th.no_grad():
+            # Forward pass
+            eval_output = best_model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+        predictions = eval_output.logits.detach().cpu().numpy()
+        predictions = np.argmax(predictions, axis=1).flatten()
+        label_ids = b_labels.to('cpu').numpy()
+        # Calculate validation metrics
+        b_accuracy, b_precision, b_recall, b_f1 = metrics(label_ids, predictions)
+        batch_accuracy.append(b_accuracy)
+        batch_precision.append(b_precision)
+        batch_recall.append(b_recall)
+        batch_f1.append(b_f1)
+
+    test_output['accuracy'] = np.array(batch_accuracy).mean()
+    test_output['precision'] = np.array(batch_precision).mean()
+    test_output['recall'] = np.array(batch_recall).mean()
+    test_output['f1'] = np.array(batch_f1).mean()
+
+    print('\n\nTest results:')
+    print(f'Accuracy: {test_output["accuracy"]:.3f}')
+    print(f'Precision: {test_output["precision"]:.3f}')
+    print(f'Recall: {test_output["recall"]:.3f}')
+    print(f'F1: {test_output["f1"]:.3f}')
 
     # Save output
-    with open('./output/bert_output.json', 'w') as f:
-        json.dump(output, f)
+    with open('./output/bert_output_train.json', 'w') as f:
+        json.dump(val_output, f)
+    with open('./output/bert_output_test.json', 'w') as f:
+        json.dump(test_output, f)
 
 
 if __name__ == '__main__':
